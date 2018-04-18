964e56e8e107ae7a478fa001da026c924dceeb5f
Gregpack <gregpack0@gmail.com>
2018-04-18 20:08:25 +0700

    now works (it didn`t)

---
+++ 6)quicksort/main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void sort (int *arr, int first, int last) {
+    int a;
+    int temp, i, j;
+    while (last - first >= 1) {
+        a = arr[(last + first) / 2];
+        i = first, j = last;
+        {
+            do {
+                while (arr[i] < a)
+                    i++;
+                while (a < arr[j])
+                    j--;
+                if (i <= j){
+                    temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                    i++;
+                    j--;
+                }
+            } while (i < j);
+            if (j - first > last - i){
+                sort (arr, i, last);
+                last = j;
+            }
+            else {
+                sort (arr, first, j);
+                first = i;
+            }
+        }
+    }
+}
+
+int main() {
+    long long int i, n;
+    int * arr;
+    scanf("%lld", &n);
+    arr = (int *) malloc(n * sizeof(int));
+    for (i = 0; i < n; i++) {
+        scanf ("%d", &arr[i]);
+    }
+    sort (arr, 0, n - 1);
+    for (i = 0; i < n; i++) {
+        printf ("%d ", arr[i]);
+    }
+    free (arr);
+    return 0;
+}
